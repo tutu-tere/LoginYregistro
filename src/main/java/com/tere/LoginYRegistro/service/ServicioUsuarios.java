5933f203b3419eaa591d1ffb50ed6d8da7c016d5
Tere <teresaolivaresrueda@gmail.com>
2026-06-30 15:01:35 -0400

    password hasheado

---
+++ src/main/java/com/tere/LoginYRegistro/service/ServicioUsuarios.java
@@ -0,0 +1,38 @@
+package com.tere.LoginYRegistro.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.stereotype.Service;
+
+import com.tere.LoginYRegistro.models.Usuario;
+import com.tere.LoginYRegistro.repository.RepositorioUsuarios;
+
+@Service
+
+public class ServicioUsuarios {
+
+  @Autowired
+  private RepositorioUsuarios repoUsuarios;
+
+  public Usuario regristrar(Usuario nuevoUsuario, BindingResult result) {
+    String password = nuevoUsuario.getPassword();
+    String confirmacion = nuevoUsuario.getConfirmacion();
+    if (!password.equals(confirmacion)) {
+      result.rejectValue("confirmacion", "Matches", "Los dos campos no coinciden");
+    }
+    String email = nuevoUsuario.getEmail();
+    Usuario existeUsuario = repoUsuarios.findByEmail(email);
+    if (existeUsuario != null) {
+      result.rejectValue("email", "unique", "El email ya esta registrado, elija otro");
+    }
+    if (result.hasErrors()) {
+      return null;
+    } else {
+      String passwordHasheado = BCrypt.hashpw(password, BCrypt.gensalt());
+      nuevoUsuario.setPassword(passwordHasheado);
+
+      return repoUsuarios.save(nuevoUsuario);
+    }
+  }
+}
